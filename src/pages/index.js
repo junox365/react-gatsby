8cbc07270c560395417618f2d45f24b32826819c
junox365 <junox365@gmail.com>
2019-10-14 23:17:34 +0900

    about, contract added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
-
-export default () => <div>Hello Gatsby!</div>
+import { Link } from "gatsby"
+import Header from "../components/header"
+export default () => (
+    <div style={{ color : `purple`}}>
+        <Link to="/contract/">Contract</Link>
+        <Link to="/about/">About</Link>
+        <Header headerText="Hello Gatsby!" />
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+)
